a34d857fb4c45fa3ef9960e9c9b8e417937a964b
Vauchi CI <ci@vauchi.com>
2026-01-31 08:31:30 +0000

    chore: Update to v0.1.3

--- vauchi-mobile/src/main/kotlin/com/vauchi/mobile/VauchiMobile.kt
+++ vauchi-mobile/src/main/kotlin/com/vauchi/mobile/VauchiMobile.kt
@@ -21,7 +21,7 @@ object VauchiMobileLib {
     /**
      * Library version
      */
-    const val VERSION = "0.1.2"
+    const val VERSION = "0.1.3"
 
     /**
      * Check if native library is loaded
